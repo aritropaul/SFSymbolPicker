c4ad6f7f775165f6076d9c37510ae3a37863afb0
Aritro Paul <aritro.paul61@gmail.com>
2020-06-24 17:00:30 +0530

    Initial Commit

---
+++ SFSymbolPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFSymbolPicker
+//
+//  Created by Aritro Paul on 24/06/20.
+//  Copyright © 2020 Aritro Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
